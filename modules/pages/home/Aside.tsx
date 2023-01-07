ee09d4659eadd7c86a9089497d81c4e07b696070
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-01-07 10:25:23 +0100

    Display similar media on media page

--- modules/pages/home/Aside.tsx
+++ modules/pages/home/Aside.tsx
@@ -3,7 +3,7 @@ import PeopleMedias from "@/components/medias/People";
 
 const Aside: FC = () => {
   return (
-    <aside className="overflow-x-hidden px-2 mt-4 md:pl-4 xl:pl-2 xl:bg-black-2 xl:mt-0 xl:h-screen xl:sticky xl:top-0 xl:right-0 xl:flex xl:flex-col">
+    <aside className="aside">
       <h3 className="xl:mb-1">Popular People</h3>
       <PeopleMedias />
     </aside>
