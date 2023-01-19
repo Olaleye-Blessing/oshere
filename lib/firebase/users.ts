bee079dda6c8085a319916bcc97300d162ae28c6
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-01-19 04:08:46 +0100

    Enable users to leave a community

--- lib/firebase/users.ts
+++ lib/firebase/users.ts
@@ -1,4 +1,4 @@
-import { arrayUnion, updateDoc } from "firebase/firestore";
+import { arrayRemove, arrayUnion, updateDoc } from "firebase/firestore";
 import { getDocumentRef } from "./general";
 
 type GeneralData = {
@@ -12,16 +12,25 @@ type DataString = {
 
 type DataArray = {
   type: "array";
+  nature: "add" | "remove";
 };
 
 type UserData = GeneralData & (DataString | DataArray);
+
+const getArrayData = (
+  nature: DataArray["nature"],
+  value: GeneralData["value"]
+) => (nature === "add" ? arrayUnion(value) : arrayRemove(value));
 
 export const updateUserData = async (userId: string, data: UserData) => {
   try {
     const userRef = getDocumentRef(`users/${userId}`);
 
     await updateDoc(userRef, {
-      [data.key]: data.type === "string" ? data.value : arrayUnion(data.value),
+      [data.key]:
+        data.type === "string"
+          ? data.value
+          : getArrayData(data.nature, data.value),
     });
   } catch (error: any) {
     throw new Error(error);
