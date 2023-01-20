87303ae2433e791de21168fee8e204b339b40d4a
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-01-20 08:24:19 +0100

    Create seperate components for media and person

--- components/medias/People.tsx
+++ components/medias/People.tsx
@@ -1,10 +1,8 @@
-import { TMDBPerson } from "interfaces/fetch";
-import Image from "next/image";
-import Link from "next/link";
 import { FC } from "react";
-import LoadingIndicator from "../LoadingIndicator";
-import { useTMDBPagination } from "./../../hooks/useTMDBPagination";
-import { TMDB_IMAGE_BASE_URL } from "configs/paths";
+import LoadingIndicator from "@/components/LoadingIndicator";
+import { useTMDBPagination } from "@/hooks/useTMDBPagination";
+import { TMDBPerson } from "@/interfaces/fetch";
+import Person from "./Person";
 
 const People: FC = () => {
   const { data, error, loading, fetchMore } =
@@ -18,33 +16,7 @@ const People: FC = () => {
           className="mt-3 flex whitespace-nowrap items-center justify-start overflow-x-scroll scrollbar__hide space-x-5 pr-5 sm:space-x-8 xl:flex-col xl:space-x-0 xl:gap-y-5 xl:px-2 xl:overflow-x-hidden xl:scrollbar__show"
         >
           {data?.results.map((person) => {
-            return (
-              <li key={person.id} className="w-full">
-                <Link
-                  href="/"
-                  className="flex w-full max-w-[22rem] pr-4 rounded-lg bg-black bg-opacity-20 shadow-lg overflow-hidden"
-                >
-                  <figure className="w-20 relative flex-shrink-0 mr-2 overflow-hidden">
-                    <Image
-                      src={`${TMDB_IMAGE_BASE_URL}/w500${person.profile_path}`}
-                      alt=""
-                      width={500}
-                      height={500}
-                      className="h-full object-cover"
-                    />
-                  </figure>
-                  <div className="truncate">
-                    <h4 className="truncate mb-0">{person.name}</h4>
-                    <p className="text-white-primary text-opacity-50 font-semibold text-sm">
-                      {person.known_for_department}
-                    </p>
-                    <p className="mt-6 bg-red bg-opacity-10 text-red-primary max-w-max p-1 font-bold text-xs rounded-sm mb-2">
-                      {person.popularity}
-                    </p>
-                  </div>
-                </Link>
-              </li>
-            );
+            return <Person key={person.id} person={person} />;
           })}
         </ul>
       )}
