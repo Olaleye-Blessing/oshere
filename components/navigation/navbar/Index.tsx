1ad6c8f7b05fe6d0dc3c8f3a1806c41742980ae5
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-01-20 08:26:25 +0100

    Enable users to search for media

--- components/navigation/navbar/Index.tsx
+++ components/navigation/navbar/Index.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import HomeLogo from "@/components/HomeLogo";
-import { categoryPages, generalPages } from "./utlis";
 import Auth from "./Auth";
-
-const navPages = [...generalPages, ...categoryPages];
+import Pages from "./Pages";
+import Search from "./Search";
 
 const Index = () => {
   const pathname = usePathname();
@@ -39,41 +37,9 @@ const Index = () => {
         ref={navLists}
         className="navbar__list--cont"
       >
-        <ul className="mt-4">
-          {navPages.map(({ header, pages }) => {
-            return (
-              <li key={header} className="mb-5 md:mb-10">
-                <h5 className="mb-1">{header}</h5>
-                <ul>
-                  {pages.map((page) => {
-                    return (
-                      <li key={page.label} className="mb-2 md:mb-3">
-                        <Link
-                          href={page.path}
-                          className={`flex items-center justify-start ${
-                            pathname === page.path
-                              ? "text-red-primary"
-                              : "hover:text-red-light"
-                          }`}
-                        >
-                          <span className="inline-block w-4 h-4 mr-1 mt-1">
-                            {pathname === page.path ? (
-                              <page.activeIcon />
-                            ) : (
-                              <page.icon />
-                            )}
-                          </span>
-                          <span>{page.label}</span>
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
+        <Pages />
         <Auth />
+        <Search />
       </div>
     </nav>
   );
