827d6acfa525c8c3cb87bf0b4bb50a534f7db719
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-01-22 13:39:26 +0100

    Add support for TypeScript in Jest
    This commit adds support for TypeScript in Jest as found here: https://github.com/testing-library/jest-dom#with-typescript

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/configs/(.*)$": "<rootDir>/configs/$1",
