235f2bad7c0bf714c0d7714afc65f36eead43965
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-02-04 17:23:26 +0100

    Add font family

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,12 @@
 import { FC, PropsWithChildren } from "react";
 import { usePathname } from "next/navigation";
+import { Inter } from "@next/font/google";
 import Navbar from "@/components/navigation/navbar/Index";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   const pathname = usePathname();
@@ -8,15 +14,17 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   const twoColumnLayout = ["/communities", "/search"];
 
   return (
-    <div
-      className={
-        twoColumnLayout.some((path) => String(pathname).includes(path))
-          ? "layout__two"
-          : "layout__three"
-      }
-    >
-      <Navbar />
-      {children}
+    <div className={`${inter.variable} font-sans`}>
+      <div
+        className={
+          twoColumnLayout.some((path) => String(pathname).includes(path))
+            ? "layout__two"
+            : "layout__three"
+        }
+      >
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,11 @@ module.exports = {
     "./modules/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
